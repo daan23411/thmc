52dea3b5d883c35d8ac25e56717d85449f98dbd6
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-08-04 15:58:06 +0200

    Update giveaway.js

--- commands/fun/giveaway.js
+++ commands/fun/giveaway.js
@@ -1,30 +1,32 @@
-const {MessageEmbed} = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 const ms = require('ms')
-module.exports={
+const { isValidObjectId } = require('mongoose')
+module.exports = {
     name: 'giveaway',
     description: 'Maak een giveaway!',
     usage: '<time> <prize>',
     category: 'fun',
-    run: async(bot,message,args)=>{
-        if(!message.member.permissions.has("ADMINISTRATOR")) return message.reply("Je hebt geen permissie om dit te doen")
-        let timev = message.content.slice(bot.prefix.length+9)
-        if(!timev) return message.reply('Geen tijd opgegeven in MS.')
-        let time = parseInt(timev,10)
-        if(time<= 15000){
-            return message.reply('Je tijd is minder dan 15s. Maak de tijd groter')
-        }
-        let prize = message.content.split(`${time}`).join("").split(`${bot.prefix}giveaway `).join("")
-        if(!prize) return message.reply("Geen prijs opgegeven.")
-        const Embed = new MessageEmbed()
+    run: async (bot, message, args) => {
+        if(!args[0]) return message.channel.send('Geen tijd opgegeven!')
+        if(!args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")) return message.channel.send('Je hebt de tijd niet goed opgegeven. Het moet in uren minuten of dagen.')
+        if(isNaN(args[0][0])) return message.channel.send('Dat is geen getal.')
+        let channel = message.mentions.channels.first()
+        if(!channel) return message.channel.send('Ik kan dat kanaal niet vinden.')
+        let prize = args.slice(2).join(" ")
+        if(!prize) return message.channel.send('Geen prijs opgegeven.')
+        message.channel.send(`*Giveaway gestart in ${channel}*`)
+        let embed = new MessageEmbed()
         .setTitle('Nieuwe Giveaway')
-        .setDescription(prize)
+        .setDescription(`De prijs is **${prize}**`)
+        .setTimestamp(Date.now()+ms(args[0]))
+        .setFooter(`Giveaway gestart door ${message.author}`)
         .setColor('RANDOM')
-        .setFooter(`Deze giveaway duurt ${ms(time)}!`)
-        let msg = await message.channel.send(Embed)
-        await msg.react('🎉')
+        let m = await channel.send(embed)
+        m.react("🎉")
         setTimeout(() => {
-           let winner = msg.reactions.cache.get("🎉").users.cache.random().id
-           message.channel.send(`De winnaar is <@${winner}> \nBedankt voor het meedoen @everyone!`)
-        }, time);
+            if(m.reactions.cache.size<=1) return channel.send('Er doen te weinig mensen mee aan de giveaway om een winnaar uit te zoeken.')
+            let winner = m.reactions.cache.get("🎉").users.cache.filter(u=>!u.bot).random()
+            channel.send(`De winnaar van **${prize}** is geworden... ${winner}`)
+        }, ms(args[0]));
     }
 }
